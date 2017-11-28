4af870037fd3514650409430322039462aa69fd7
img21326 <img21326@gmail.com>
2017-11-28 22:08:39 +0800

    4.	設定 route 及對應的 Controller

--- routes/web.php
+++ routes/web.php
@@ -10,19 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('posts', ['as' => 'posts.index', 'uses' => 'PostsController@index']);
+Route::get('post', ['as' => 'posts.show', 'uses' => 'PostsController@show']);
+Route::get('about', ['as' => 'posts.about', 'uses' => 'PostsController@about']);
+Route::get('contact', ['as' => 'posts.contact', 'uses' => 'PostsController@contact']);
 
-Route::get('/', function () {
-    return view('index');
-});
-
-Route::get('/about', function () {
-    return view('about');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-Route::get('/post', function () {
-    return view('post');
-});
